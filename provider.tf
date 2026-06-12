455941c51dffa8b144e395d87e81059423ab14cb
utpal <utpal.it@gmail.com>
2026-06-12 05:57:22 +0000

    terraform project

--- provider.tf
+++ provider.tf
@@ -20,6 +20,14 @@ provider "azurerm" {
   features {}
 }*/
 
+provider "azurerm" {
+  # resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
 variable "client_id" {
   type = string
 }

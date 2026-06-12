aa64888b762b94cc1a5a90bbf2c6396e6b286328
utpal <utpal.it@gmail.com>
2026-06-12 06:52:03 +0000

    modified instance type

--- a7.linux-vm-.tf
+++ a7.linux-vm-.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = "${local.resource_name_prefix}-linux-vm-${each.key}"
   resource_group_name = azurerm_resource_group.my_rg.name
   location            = azurerm_resource_group.my_rg.location
-  size                = var.vm_instance_size["test"] #it will look for the value of the key in the map and return the value
+  size                = var.vm_instance_size["dev"] #it will look for the value of the key in the map and return the value
   admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.web_nic[each.key].id,
